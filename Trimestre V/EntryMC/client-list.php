38481b64575be149356f7f62ff375d9832a9b088
sebastianA10 <91230510+Mmartinez951@users.noreply.github.com>
2022-08-24 21:24:53 -0500

    Error

--- Trimestre V/EntryMC/client-list.php
+++ Trimestre V/EntryMC/client-list.php
@@ -1,11 +1,50 @@
 <?php
 include("./Conexion/Conexion.php");
-
+if($_POST){
+    $obj->nombreUsuario = $_POST['Nombre_Usuario'];
+}
 $cone = new Conexion();
 $c = $cone -> conectando();
-$query = "SELECT * FROM USUARIOS";
+
+$queryCantUsuarios = "SELECT COUNT(*) AS TotalRegistros FROM usuarios";
+$ejecuta = mysqli_query($c,$queryCantUsuarios);
+$TotalRegistros = mysqli_fetch_array($ejecuta)['TotalRegistros'];
+
+$maximoRegistros = 10;
+if(empty($_GET['pagina'])){
+	$pagina=1;
+}else{
+	$pagina=$_GET['pagina'];
+}
+$desde = ($pagina-1)*$maximoRegistros;
+$totalRegistros=ceil($TotalRegistros/$maximoRegistros);
+
+if(isset($_POST['buscarUsuario'])){
+    $query="SELECT Id_Usuario, Nombre_Usuario, Apellido_Usuario, R.Nombre_Rol, Nombre_Documento, Numero_Documento, Direccion, Correo_Electronico, Celular, Login, Password
+	FROM usuarios U 
+	INNER JOIN tipo_documentos TP ON U.Tipo_Documento = TP.Id_Tipo_Documento 
+	INNER JOIN roles R ON U.Nombre_Rol = R.Id_Rol WHERE U.Nombre_Usuario like '%$obj->nombreUsuario%' limit $desde, $maximoRegistros";
+    $ejecuta = mysqli_query($c,$query);
+    $usuarios = mysqli_fetch_array($ejecuta);
+}else{$query="SELECT Id_Usuario, Nombre_Usuario, Apellido_Usuario, R.Nombre_Rol, Nombre_Documento, Numero_Documento, Direccion, Correo_Electronico, Celular, Login, Password
+	FROM usuarios U 
+	INNER JOIN tipo_documentos TP ON U.Tipo_Documento = TP.Id_Tipo_Documento 
+	INNER JOIN roles R ON U.Nombre_Rol = R.Id_Rol ORDER BY U.Id_Usuario limit $desde,$maximoRegistros";
+    $ejecuta = mysqli_query($c,$query);
+    $usuarios = mysqli_fetch_array($ejecuta);
+}
+
+
+$query = "SELECT Id_Usuario, Nombre_Usuario, Apellido_Usuario, R.Nombre_Rol, Nombre_Documento, Numero_Documento, Direccion, Correo_Electronico, Celular, Login, Password
+FROM usuarios U 
+INNER JOIN tipo_documentos TP ON U.Tipo_Documento = TP.Id_Tipo_Documento 
+INNER JOIN roles R ON U.Nombre_Rol = R.Id_Rol ORDER BY U.Id_Usuario limit $desde,$maximoRegistros" ;
+
 $ejecuta = mysqli_query($c,$query);
 $usuarios = mysqli_fetch_array ($ejecuta);
+
+
+/* echo $TotalRegistros; */
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -150,26 +189,43 @@ $usuarios = mysqli_fetch_array ($ejecuta);
 			<!-- Page header -->
 			<div class="full-box page-header">
 				<h3 class="text-left">
-					<i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE CLIENTES
+					<i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE USUARIOS
 				</h3>
 				<p class="text-justify">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nostrum rerum animi natus beatae ex. Culpa blanditiis tempore amet alias placeat, obcaecati quaerat ullam, sunt est, odio aut veniam ratione.
+					GESTIÓN DE USUARIOS DE LA PLATAFORMA EntryMC 
 				</p>
 			</div>
 
 			<div class="container-fluid">
 				<ul class="full-box list-unstyled page-nav-tabs">
 					<li>
-						<a href="client-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; AGREGAR CLIENTE</a>
+						<a href="client-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; AGREGAR USUARIO</a>
 					</li>
 					<li>
-						<a class="active" href="client-list.php"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE CLIENTES</a>
-					</li>
-					<li>
-						<a href="client-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; BUSCAR CLIENTE</a>
+						<a class="active" href="client-list.php"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE USUARIOS</a>
 					</li>
 				</ul>	
 			</div>
+			<div class="container-fluid">
+				<form class="form-neon" action="">
+					<div class="container-fluid">
+						<div class="row justify-content-md-center">
+							<div class="col-12 col-md-6">
+								<div class="form-group">
+									<label for="inputSearch" class="bmd-label-floating">¿Qué cliente estas buscando?</label>
+									<input type="text" class="form-control" name="buscarUsuario" id="inputSearch" maxlength="30">
+								</div>
+							</div>
+							<div class="col-12">
+								<p class="text-center" style="margin-top: 40px;">
+									<button type="submit" name="buscarUsuario" id="buscarUsuario" class="btn btn-raised btn-info"><i class="fas fa-search"></i> &nbsp; BUSCAR</button>
+								</p>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+			
 			
 			<!-- Content here-->
 			<div class="container-fluid">
@@ -180,6 +236,8 @@ $usuarios = mysqli_fetch_array ($ejecuta);
 								<th>#</th>
 								<th>Nombre</th>
 								<th>Apellido</th>
+								<th>Rol</th>
+								<th>Tipo Documento</th>
 								<th>Numero Documento</th>
 								<th>Dirección</th>
 								<th>Correo Electrónico</th>
@@ -203,17 +261,20 @@ $usuarios = mysqli_fetch_array ($ejecuta);
 								<td><?php echo $usuarios[0]?></td>
 								<td><?php echo $usuarios[1]?></td>
 								<td><?php echo $usuarios[2]?></td>
+								<td><?php echo $usuarios[3]?></td>
 								<td><?php echo $usuarios[4]?></td>
 								<td><?php echo $usuarios[5]?></td>
 								<td><?php echo $usuarios[6]?></td>
 								<td><?php echo $usuarios[7]?></td>
+								<td><?php echo $usuarios[8]?></td>
 								<td><?php echo $usuarios[9]?></td>
 								<td><?php echo $usuarios[10]?></td>
-									
+							
+							
 								</td>
 								<td>
 									<a href="client-update.html" class="btn btn-success">
-	  									<i class="fas fa-sync-alt"></i>	
+									<i class="fas fa-edit"></i>
 									</a>
 								</td>
 								<td>
@@ -233,18 +294,42 @@ $usuarios = mysqli_fetch_array ($ejecuta);
 				</div>
 				<nav aria-label="Page navigation example">
 					<ul class="pagination justify-content-center">
-						<li class="page-item disabled">
-							<a class="page-link" href="#" tabindex="-1">Previous</a>
-						</li>
-						<li class="page-item"><a class="page-link" href="#">1</a></li>
-						<li class="page-item"><a class="page-link" href="#">2</a></li>
-						<li class="page-item"><a class="page-link" href="#">3</a></li>
-						<li class="page-item">
-							<a class="page-link" href="#">Next</a>
+					<?php 
+                    if($pagina!=1){
+                    ?>
+                    <li class="page-item ">
+                        <a class="page-link" href="?pagina=<?php echo 1; ?>"><</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="?pagina=<?php echo $pagina-1; ?>"><<</a>
+                    </li>
+                    <?php
+                    }
+                    for($i=1; $i<=$totalRegistros; $i++){
+                        if($i==$pagina){
+                            echo'<li class="page-item active" aria-current="page"><a class="page-link" href="?pagina='.$i.'">'.$i.'</a></li>';    
+                        }
+                        else{
+                            echo'<li class="page-item "><a class="page-link" href="?pagina='.$i.'">'.$i.'</a></li>'; 
+                        }
+                    }
+                    if($pagina !=$totalRegistros){
+                    ?>
+                    
+                    <li class="page-item">
+                        <a class="page-link" href="?pagina=<?php echo $pagina+1; ?>">>></a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="?pagina=<?php echo $totalRegistros; ?>">></a>
+                    </li>
+                    <?php
+                    }
+                    ?>
 						</li>
 					</ul>
 				</nav>
 			</div>
+			
 
 		</section>
 	</main>
